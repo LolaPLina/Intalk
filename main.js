9101bde44eabdd38dfc9bea37483cff4d942e5a3
LolaPLina <131884228+LolaPLina@users.noreply.github.com>
2024-04-01 09:49:08 -0300

    main.js

--- main.js
+++ main.js
@@ -1,18 +1,8 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBLlftrGeZDouq1H4WENi_ej6cjb4X4rG0",
-    authDomain: "instalk-5d7a2.firebaseapp.com",
-    databaseURL: "https://instalk-5d7a2-default-rtdb.firebaseio.com",
-    projectId: "instalk-5d7a2",
-    storageBucket: "instalk-5d7a2.appspot.com",
-    messagingSenderId: "282136429170",
-    appId: "1:282136429170:web:02a81a2bee007544c6b8eb"
-  };
-
-  function addUser() {
+function addUser() {
 
     userName = document.getElementById("userName").value;
   
     localStorage.setItem("userName", userName);
     
-    window.location = "kwitterRoom.html";
-  }
+    window.location = "instalkRoom.html";
+}
